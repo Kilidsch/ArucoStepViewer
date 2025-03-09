f079dff6fcf4057612af4867a42ed5cbe78bdb4f
Deniz Kilic <deniz-mo@hotmail.de>
2025-03-09 12:49:42 +0100

    Per tab image stack

--- src/sceneview.cpp
+++ src/sceneview.cpp
@@ -7,6 +7,7 @@
 #include <FL/fl_draw.H>
 #include <FL/platform.H>
 #include <iostream>
+
 SceneView::SceneView(int x, int y, int w, int h) : Fl_Tabs(x, y, w, h)
 {
     auto &test_images = TestImages::getInstance();
@@ -18,19 +19,87 @@ SceneView::SceneView(int x, int y, int w, int h) : Fl_Tabs(x, y, w, h)
     int ww = 0, hh = 0; // initialize to zero before calling fl_measure()
     fl_measure("test text", ww, hh);
 
-    for (const auto &tab : m_tabs)
+    for (auto &tab : m_tabs)
     {
         int rx, ry, rw, rh;
         this->client_area(rx, ry, rw, rh, hh);
-        auto *group = new Fl_Group(rx, ry, rw, rh, "Test");
-        group->copy_label(tab.name.c_str());
-        auto *pic_box = new Fl_Box(0, 0, rw, rh);
-        auto *img = new Fl_RGB_Image(tab.imgs.back().data, tab.imgs.back().cols, tab.imgs.back().rows,
-                                     tab.imgs.back().channels(), static_cast<int>(tab.imgs.back().step));
-        pic_box->image(img);
-        group->end();
-        group->resizable(pic_box);
-        this->resizable(group); // overwrites all the time, but we dont care
+        auto stack = new ImageStack(std::move(tab), rx, ry, rw, rh);
+        this->resizable(stack);
     }
     this->end();
+}
+
+ImageStack::ImageStack(Tab &&initial_tab, int x, int y, int w, int h) : Fl_Tabs(x, y, w, h)
+{
+    m_tab = std::move(initial_tab);
+    this->copy_label(m_tab.name.c_str());
+    this->handle_overflow(Fl_Tabs::OVERFLOW_PULLDOWN);
+    int ww = 0, hh = 0; // initialize to zero before calling fl_measure()
+    fl_measure("test text", ww, hh);
+
+    m_groups.resize(m_tab.imgs.size());
+    for (size_t i = 0; i < m_tab.imgs.size(); ++i)
+    {
+        int rx, ry, rw, rh;
+        this->client_area(rx, ry, rw, rh, hh);
+        m_groups[i] = new Fl_Group(rx, ry, rw, rh, "Test");
+        m_groups[i]->copy_label(std::to_string(i).c_str());
+        const auto &img = m_tab.imgs[i];
+        auto pic_box = new Fl_Box(0, 0, w, h);
+        auto *img_widget = new Fl_RGB_Image(img.data, img.cols, img.rows, img.channels(), static_cast<int>(img.step));
+        pic_box->image(img_widget);
+        m_groups[i]->end();
+        m_groups[i]->resizable(pic_box);
+
+        // only last iteration does not get overwritten; but this is fine
+        this->resizable(m_groups[i]);
+    }
+    this->end();
+}
+
+void ImageStack::setTab(Tab &&tab)
+{
+    if (tab.imgs.size() < m_groups.size())
+    {
+        for (size_t i = tab.imgs.size(); i < m_groups.size(); i++)
+        {
+            // since FLTK 1.3 schedules removal from parent as well
+            delete m_groups[i];
+        }
+        // NOTE: need to make a still existing group resizable?
+        m_groups.resize(tab.imgs.size());
+    }
+    else if (tab.imgs.size() > m_groups.size())
+    {
+        m_groups.resize(tab.imgs.size());
+        this->begin();
+        for (size_t i = m_groups.size(); i < tab.imgs.size(); ++i)
+        {
+            // TODO: extra add tab function
+            int rx, ry, rw, rh;
+            this->client_area(rx, ry, rw, rh, 10);
+            m_groups[i] = new Fl_Group(rx, ry, rw, rh, "Test");
+            m_groups[i]->copy_label(std::to_string(i).c_str());
+            auto pic_box = new Fl_Box(0, 0, 10, 10);
+            m_groups[i]->end();
+            m_groups[i]->resizable(pic_box);
+
+            // only last iteration does not get overwritten; but this is fine
+            this->resizable(m_groups[i]);
+        }
+        this->end();
+    }
+
+    for (size_t i = 0; i < m_groups.size(); i++)
+    {
+        Fl_Box *pic_box = dynamic_cast<Fl_Box *>(m_groups[i]->child(0));
+        auto old_image = pic_box->image();
+        const auto &img = tab.imgs[i];
+        auto *img_widget = new Fl_RGB_Image(img.data, img.cols, img.rows, img.channels(), static_cast<int>(img.step));
+        pic_box->image(img_widget);
+        delete old_image;
+    }
+
+    // at the end so that all images exist as long as they are referenced by a widget
+    m_tab = std::move(tab);
 }
--- src/sceneview.h
+++ src/sceneview.h
@@ -5,6 +5,8 @@
 
 #include <testimages.h>
 
+class Fl_Box;
+
 class SceneView : public Fl_Tabs
 {
   public:
@@ -12,6 +14,17 @@ class SceneView : public Fl_Tabs
 
   private:
     std::vector<Tab> m_tabs;
+};
+
+class ImageStack : public Fl_Tabs
+{
+    Tab m_tab;
+    std::vector<Fl_Group *> m_groups;
+
+  public:
+    ImageStack(Tab &&initial_tab, int x, int y, int w, int h);
+
+    void setTab(Tab &&tab);
 };
 
 #endif // SCENEVIEW_H
